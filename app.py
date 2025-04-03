6490e3a724aaaf596627e206521541124552beeb
Dhruv Bafna (Jain) <86953362+DhruvBafna777@users.noreply.github.com>
2025-04-04 00:14:25 +0530

    Update app.py

--- app.py
+++ app.py
@@ -37,7 +37,7 @@ st.set_page_config(
 st.markdown("""
     <style>
     .stApp {
-        background-color: #f0f2f6;
+        background-color: #000000;
     }
     .chat-message {
         padding: 1.5rem;
